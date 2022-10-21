96f73a377cee788b2edf0e5dea41248bd9a4f058
MashNjogu <dnjogu47@gmail.com>
2022-10-21 16:52:21 +0300

    Added AddNote Screen

--- lib/utils/drawer_items.dart
+++ lib/utils/drawer_items.dart
@@ -11,17 +11,27 @@ class DrawerItems extends StatelessWidget {
         SizedBox(
           height: size.height * 0.085,
         ),
-        Text("Google Keep"),
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.lightbulb_outline), Text("Notes")],
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Align(
+              alignment: Alignment.bottomLeft, child: Text("Google Keep")),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 14.0, bottom: 10, top: 10),
+          child: GestureDetector(
+            child: Row(
+              children: [
+                Icon(Icons.lightbulb_outline),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("Notes"),
+                )
+              ],
+            ),
           ),
         ),
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.notifications_outlined), Text("Reminders")],
-          ),
-        ),
+        paddingOnDrawerItems("Reminders", Icons.notifications_outlined, () {}),
         // Divider(
         //   thickness: 2,
         // ),
@@ -29,35 +39,31 @@ class DrawerItems extends StatelessWidget {
         //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
         //   children: [Text("Label"), Text("Edit")],
         // )
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.add), Text("Create new label")],
-          ),
-        ),
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.archive_outlined), Text("Archive")],
-          ),
-        ),
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.delete_outline_sharp), Text("Delete")],
-          ),
-        ),
-        GestureDetector(
-          child: Row(
-            children: [Icon(Icons.settings), Text("Settings")],
-          ),
-        ),
-        GestureDetector(
-          child: Row(
-            children: [
-              Icon(Icons.help_outline_rounded),
-              Text("Help & feedback")
-            ],
-          ),
-        ),
+        paddingOnDrawerItems("Create new Label", Icons.add, () {}),
+        paddingOnDrawerItems("Archive", Icons.archive_outlined, () {}),
+        paddingOnDrawerItems("Delete", Icons.delete_outline_sharp, () {}),
+        paddingOnDrawerItems("Settigs", Icons.settings, () {}),
+        paddingOnDrawerItems(
+            "Help & feedback", Icons.help_outline_rounded, () {})
       ],
+    );
+  }
+
+  Padding paddingOnDrawerItems(String text, IconData icon, Function onTapped) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 14.0, bottom: 10, top: 10),
+      child: GestureDetector(
+        onTap: () => onTapped,
+        child: Row(
+          children: [
+            Icon(icon),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(text),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/bottom_nav_screens/new_note.dart
+++ lib/views/bottom_nav_screens/new_note.dart
@@ -11,10 +11,50 @@ class NewNote extends StatefulWidget {
 class _NewNoteState extends State<NewNote> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text("NewNote"),
+        body: Padding(
+      padding: const EdgeInsets.only(left: 12.0, right: 8.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.055,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              children: [
+                GestureDetector(child: Icon(Icons.arrow_back)),
+                Spacer(),
+                GestureDetector(child: Icon(Icons.push_pin_outlined)),
+                SizedBox(
+                  width: 15.0,
+                ),
+                GestureDetector(child: Icon(Icons.notification_add_outlined)),
+                SizedBox(
+                  width: 15.0,
+                ),
+                GestureDetector(child: Icon(Icons.archive_outlined))
+              ],
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Title",
+                hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                border: InputBorder.none),
+            maxLines: null,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Note",
+                hintStyle: TextStyle(fontSize: 16),
+                border: InputBorder.none),
+            maxLines: null,
+          )
+
+        ],
       ),
-    );
+    ));
   }
 }
